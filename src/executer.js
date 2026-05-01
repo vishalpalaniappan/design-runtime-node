1745eed21d8fa0ac222c5e2c54a10b0b083f339d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-01 12:13:10 -0400

    Test interface for getting inputs from user.

--- src/executer.js
+++ src/executer.js
@@ -1,3 +1,14 @@
-export const run = (design) => {
+import { createInterface } from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
 
+const rl = createInterface({
+  input: stdin,
+  output: stdout
+});
+
+
+export const run = async (design) => {
+    const name = await rl.question('Enter name: ');
+    console.log(`You entered: ${name}`);
+    rl.close();
 }
