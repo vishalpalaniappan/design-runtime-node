462f6df0d59354f87a5c8313ca7fdf5009709e74
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-01 12:08:50 -0400

    Add methods to load design and trigger its execution.

---
+++ index.js
@@ -0,0 +1,13 @@
+import {run} from './src/executer.js';
+import {DALEngine} from "dal-engine-core-js-lib-dev";
+import {readFile} from "fs/promises";
+
+const loadDesign = async (path) => {
+    const data = await readFile(path);
+    const engine = new DALEngine({name: "Library Manager", description: ""});
+    engine.deserialize(data);
+    return engine;
+}
+
+const designEngine = await loadDesign('./designs/demo2.dal');
+run(designEngine);
