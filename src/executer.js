8833654c8b63eb10ccb6cb048087c2ce71abaf6b
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-01 14:07:29 -0400

    Add todo for plan forward.

--- src/executer.js
+++ src/executer.js
@@ -11,13 +11,23 @@ export const run = async (design) => {
     const atomic = graph.nodes.find((node) =>node.isAtomic());
 
     if (atomic) {
-      // Start at atomic node and then walk
-      // the behavioral control graph.
+      // Start at atomic node and then walk the behavioral control graph.
       console.log("Found atomic node: ", atomic.getBehavior().getName()); 
     }
     
     const name = await rl.question('Enter name: ');
     console.log(`You entered: ${name}`);
     rl.close();
+
+    /**
+     * TODO: So ultimately, this is essentially going to be
+     * an execution of the script. So when the script requires
+     * a participant as an input, then I need to request that
+     * and load it into the args. If an required input has a
+     * value, then I need to check if that world state is
+     * satisfied. So I will add a method to the behavior to parse
+     * the input requirements and then use that to execute logic
+     * before executing the script (if the behavior is valid).
+     */
 
 }
