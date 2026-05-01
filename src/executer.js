23d8cf498dc154afb5c9e322198bfad29787f60a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-01 14:04:14 -0400

    Find atomic node to begin executing behavioral control graph.

--- src/executer.js
+++ src/executer.js
@@ -6,9 +6,18 @@ const rl = createInterface({
   output: stdout
 });
 
-
 export const run = async (design) => {
+    const graph = design.graphs.getActiveGraph();
+    const atomic = graph.nodes.find((node) =>node.isAtomic());
+
+    if (atomic) {
+      // Start at atomic node and then walk
+      // the behavioral control graph.
+      console.log("Found atomic node: ", atomic.getBehavior().getName()); 
+    }
+    
     const name = await rl.question('Enter name: ');
     console.log(`You entered: ${name}`);
     rl.close();
+
 }
