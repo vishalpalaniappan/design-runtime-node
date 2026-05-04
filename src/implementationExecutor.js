653746543919de9fa225271eed6990156f761e2a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-04 18:44:15 -0400

    Add support for non interaction execution to implementation executor.

--- src/implementationExecutor.js
+++ src/implementationExecutor.js
@@ -3,39 +3,52 @@ import CollectInputs from "./collectInputs.js";
 
 const args = process.argv;
 if (args.length < 3) {
-    console.error("Please provide the path to the design file as an argument.");
-    process.exit(1);
+	console.error("Please provide the path to the design file as an argument.");
+	process.exit(1);
 }
-
 
 const path = args[2];
 const trace = args[3];
 let inputs;
-try {
-    inputs = await CollectInputs(trace);
-} catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1);
+if (trace) {
+	try {
+		inputs = await CollectInputs(trace);
+	} catch (err) {
+		console.error(`Error: ${err.message}`);
+		process.exit(1);
+	}
 }
 
-const child = spawn("python3", [path], {
-  stdio: ["pipe", "pipe", "inherit"]
-});
+if (inputs && inputs.length > 0) {
+	// Interactive mode
+	const child = spawn("python3", [path], {
+		stdio: ["pipe", "pipe", "inherit"],
+	});
 
-let i = 0;
+	let i = 0;
 
-child.stdout.on("data", (data) => {
-  process.stdout.write(data);
+	child.stdout.on("data", (data) => {
+		process.stdout.write(data);
 
-  if (i < inputs.length) {
-    console.log("");
-    console.log("Sending input:", inputs[i].argumentValue);
-    console.log("");
-    child.stdin.write(inputs[i].argumentValue + "\n");
-    i = i + 1;
-  }
-});
+		if (inputs && i < inputs.length) {
+			console.log("");
+			console.log("Sending input:", inputs[i].argumentValue);
+			console.log("");
+			child.stdin.write(inputs[i].argumentValue + "\n");
+			i = i + 1;
+		}
+	});
 
-child.on("close", (code) => {
-  console.log(`Exited with code ${code}`);
-});
+	child.on("close", (code) => {
+		console.log(`Exited with code ${code}`);
+	});
+} else {
+	// Non-interactive mode
+	const child = spawn("python3", [path], {
+		stdio: "inherit",
+	});
+
+	child.on("close", (code) => {
+		console.log(`Exited with code ${code}`);
+	});
+}
