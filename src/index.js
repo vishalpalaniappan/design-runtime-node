d5b2b1eb84e5f67c869f476ffc969883b067bdd5
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-11 15:44:17 -0400

    Rename "semantic design language" to "design abstraction language".

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ const loadDesign = async (path) => {
  * into semantic modelling where meaning is what is being implemented, it would truly be a semantic world engine. The implementation is
  * just the connectors that allow the semantic model to interact with the world and realize its meaning in the world. 
  * 
- * I can call it a semantic design language and the substrate level connectors are what connect it to the outside world. 
+ * I can call it a design abstraction language and the substrate level connectors are what connect it to the world. 
  * For example, in a distributed system, the network would connect the semantic engine running on different machines, its 
  * literally the same design running. This does feel like the natural next step because why am I implementing the same 
  * transformations as the engine? It is already computing the transformations, I just need to enable it to interact with reality.
