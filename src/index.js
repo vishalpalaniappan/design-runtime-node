67581fed70ae7c49228a26c237804b36f0fa1c8d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-11 10:59:25 -0400

    Add todo in preparation for upcoming changes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {run} from './designExecutor.js';
+import {run} from './designExecutor/designExecutor.js';
 import {DALEngine} from "dal-engine-core-js-lib-dev";
 import {readFile} from "fs/promises";
 import CollectInputs from "./collectInputs.js";
@@ -14,6 +14,18 @@ if (args.length < 3) {
     console.error("Please provide the path to the design file as an argument.");
     process.exit(1);
 }
+
+/**
+ * TODO:
+ * Add option to run the following
+ * 1. Design with inputs from user
+ * 2. Design with inputs from trace file (for replaying traces)
+ * 3. Implementation with inputs from trace file (for replaying traces)
+ * 
+ * For completeness, I guess I could also let them run the implementation with user inputs
+ * from this tool. Although that is simply running the python program because it will generate
+ * the trace file on its own. Eventually all of this will be moved to the engine.
+ */
 
 const path = args[2];
 const trace = args[3];
